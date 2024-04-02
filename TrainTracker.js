89ac315f873a1a41232ebb9e889da9ee292fa21f
Fabian Roehr <fabianroehr@me.com>
2024-04-02 15:42:40 +0200

    Initial Commit

---
+++ TrainTracker.js
@@ -0,0 +1,15 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-gray; icon-glyph: magic;
+// jshint -W119
+
+async function main() {
+	let widget = new ListWidget();
+  widget.addText('Widget Text');
+ 	let value = (config.runsInWidget) ? Script.setWidget(widget) : await widget.presentSmall();
+ 	Script.complete();
+}
+
+module.exports = {
+  main
+};
